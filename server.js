d399f5e4165959ae13553981f8baff82e25e3b09
princechisomaga <youngprince042@yahoo.com>
2024-08-23 15:03:59 +0100

    Wallet Integration basics

---
+++ server.js
@@ -0,0 +1,13 @@
+console.log("Welcome to class");
+
+function add(a, b) {
+  console.log("Sum: ", a + b);
+}
+
+add(10, 30);
+
+function multiply(a, b) {
+  console.log("Product: ", a * b);
+}
+
+multiply(3, 4);
